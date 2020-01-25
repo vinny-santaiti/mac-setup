36fc8373a11beb1b4810ccaa742c28115c194f58
Vincent Santaiti <vsantaiti@gmail.com>
2020-01-25 09:05:36 -0500

    add

---
+++ aws_boto.py
@@ -0,0 +1,20 @@
+import boto3
+
+bucket = 'bucket_name'
+prefix = 'folder_name/'
+
+client = boto3.client('s3', aws_access_key_id="access_key", aws_secret_access_key="secret_key")
+
+def list_files(client, bucket, prefix):
+    response = client.list_objects(Bucket=bucket, Prefix=prefix)
+    for content in response.get('Contents', []):
+        yield content.get('Key')
+
+files = list_files(client, bucket, prefix)
+for file in files:
+    print (file)
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(bucket)
+for obj in bucket.objects.filter(Prefix=prefix):
+    print(obj.key)
